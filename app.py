a64b11a99bc1b2da6a782d850e1b398b9a2202e9
Injae Lee <ilee.injae@gmail.com>
2024-01-19 16:16:29 -0500

    changed post to delete for modern RESTful API

--- app.py
+++ app.py
@@ -20,7 +20,10 @@ def home():
 
 @app.route("/api/books/list", methods=['GET'])
 def show_books():
-    all_books = list(db.books.find({},{"_id": False}).sort("like", -1))
+    all_books = list(
+        db.books.find(
+            {},{"_id": False})
+            .sort("like", -1))
     return jsonify({
         "result": "success", 
         "all_books": all_books
@@ -32,15 +35,26 @@ def like_book():
     title_receive = data.get("title_give")
     book = db.books.find_one({"title": title_receive})
     new_like = book["like"] + 1
-    db.books.update_one({"title": title_receive}, {"$set" : {"like": new_like}})    
-    return jsonify({"result": "success", "msg": "liked!"})
+    db.books.update_one(
+        {"title": title_receive}, 
+        {"$set" : {"like": new_like}}
+        )    
+    return jsonify(
+        {"result": "success", 
+         "msg": "liked!"}
+         )
 
-@app.route("/api/books/delete", methods=['POST'])
+@app.route("/api/books/delete", methods=['DELETE'])
 def delete_book():
     data = request.json
     title_receive = data.get("title_give")
-    db.books.delete_one({"title": title_receive})
-    return jsonify({"result": "success", "msg": "deleted!"})
+    db.books.delete_one(
+        {"title": title_receive}
+        )
+    return jsonify(
+        {"result": "success", 
+         "msg": "deleted!"}
+         )
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
